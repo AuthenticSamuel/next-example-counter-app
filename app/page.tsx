dc6178160618134f4467f75a7f0f8048e71a50b1
Samuel Ward <samuel@infernal.dev>
2024-04-08 22:11:29 +0200

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+type HomePageProps = {};
+
+const HomePage = ({}: HomePageProps) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      infernal·dev Next.js template
+    </main>
+  );
+};
+
+export default HomePage;
